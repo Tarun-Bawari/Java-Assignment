7c54a5f44fd37a375ef6eb7faea2be076e72ad06
Tarun-Bawari <52121628+Tarun-Bawari@users.noreply.github.com>
2022-07-30 12:24:58 +0530

    Update Pattern1.java

--- Pattern1.java
+++ Pattern1.java
@@ -3,7 +3,7 @@ public class Pattern1 {
 		int n=8;
 		for(int i=0; i<n; i++) {
 			for(int j=0; j<n; j++) {
-				if(j==(n-1)/2) {
+				if(i==0 || i==n-1 || j==(n-1)/2) {
 					System.out.print("* ");
 				}else {
 					System.out.print("  ");
